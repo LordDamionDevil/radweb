ddc325f8f5205b8bda0a327faa70ae1569e97bd1
LordDamionDevil <robloxfilmcam@gmail.com>
2020-03-20 21:05:12 +0100

    add docs

--- woop.js
+++ woop.js
@@ -5,6 +5,11 @@ var app = express();
 app.get("/", function (req, res) {
  res.sendFile(path.join(__dirname, '/index.html'));
 });
+app.get("/docs", function (req, res) {
+ res.sendFile(path.join(__dirname, '/docs/index.html'));
+});
 app.use('/css', express.static(path.join(__dirname, '/css')));
 app.use('/js', express.static(path.join(__dirname, '/js')));
+app.use('/assets', express.static(path.join(__dirname, '/docs/assets')));
+app.use('/search', express.static(path.join(__dirname, '/docs/search')));
 app.listen(port, function () {});
